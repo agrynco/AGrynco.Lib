88f8caaf6977e51fc2452b9ab2a51226d1c7f56c
agrynco <agrynco@gmail.com>
2015-09-01 22:54:53 +0300

    Added *.sql resources

---
+++ Sources/AGrynco.Lib/Data/DataProviders.MsSql/SQL/DropDb.sql
@@ -0,0 +1,10 @@
+﻿IF EXISTS(
+       SELECT *
+       FROM   sys.databases
+       WHERE  NAME = '@dbName'
+   )
+BEGIN
+    ALTER DATABASE [@dbName] 
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+    DROP DATABASE [@dbName]
+END
